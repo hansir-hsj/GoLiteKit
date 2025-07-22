f2ff92228794008ab8137b563629cd176adedb91
hansir-hsj <hanshijiex@163.com>
2025-07-22 15:55:33 +0800

    refactor: Restructure project directory

---
+++ core/rate_limiter_middleware.go
@@ -0,0 +1,36 @@
+package core
+
+import "net/http"
+
+func (r *RateLimiter) RateLimiterAsMiddleware(keyFunc func(r *http.Request) string) HandlerMiddleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			if r.enableGlobal && r.globalLimiter != nil {
+				if !r.globalLimiter.Allow() {
+					http.Error(w, "Too many requests", http.StatusTooManyRequests)
+					return
+				}
+			}
+
+			if keyFunc != nil {
+				key := keyFunc(req)
+				limiter := r.GetLimiter(key)
+
+				if !limiter.Allow() {
+					http.Error(w, "Too many requests", http.StatusTooManyRequests)
+					return
+				}
+			}
+
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func ByIP(r *http.Request) string {
+	return r.RemoteAddr
+}
+
+func ByPath(r *http.Request) string {
+	return r.URL.Path
+}
